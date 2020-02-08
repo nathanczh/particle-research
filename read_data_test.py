8799efb679380741a4af9e301faf4c97ce15c467
Nathan <nathan.chan.zh@gmail.com>
2020-02-08 14:08:07 -0500

    Read Data initial draft

--- read_data_test.py
+++ read_data_test.py
@@ -3,15 +3,14 @@ import read_data
 
 class TestReadData(unittest.TestCase):
     def setUp(self):
-        self.reader = read_data.DrsoscReader("../Experimental Data/Trial 21.dat")
+        self.data = read_data.read_data("../Experimental Data/Trial 21.dat")
 
     def test_read_data_works(self):
-        self.reader.read()
-        print(repr(self.reader.channels))
+        print(repr(self.data.events[0]))
 
-    def test_data_valid(self):
-        self.reader.read()
-        data = self.reader.get_data()
+    # def test_data_valid(self):
+    #     self.reader.read()
+    #     data = self.reader.get_data()
         # TODO: validate received data
 
 if __name__ == '__main__':
